adf60d3e89eae3371816c658d2d31b2293045497
Amelia <nat.horn.mk0426@gmail.com>
2019-04-20 14:09:05 +0900

    Now it can convert powerpoint slides to pdf

---
+++ PdfImageGeneratorCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ImageToPDF
+{
+    static class PdfImageGeneratorCollection
+    {
+        private static readonly PdfImageGenerator[] generators = new PdfImageGenerator[]
+        {
+            new ImageFileConverter(),
+            new PowerPointExtracter(),
+            new PdfImageExtractor()
+        };
+
+        public static void WritePdfImage(string sourceFilename)
+        {
+            var validGenerator = generators.FirstOrDefault(generator => generator.IsValidFilename(sourceFilename))
+                ?? throw new ArgumentException($"Cannot convert {sourceFilename} to PDF because of invalid format.");
+            validGenerator.WritePdfImage(sourceFilename);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,29 +10,27 @@ namespace ImageToPDF
         static void Main(string[] args)
         {
             Console.WriteLine("Images to PDF");
-            var pdfGenerators = new PdfImageGenerator[] {
-                new ImageFileConverter(),
-                new PdfImageExtractor(),
-            };
-            var filenames = getTargetFilenames(args);
-            var index = 1;
-            foreach (var path in filenames)
+            var filenames = GetTargetFilenames(args).ToArray();
+            Console.WriteLine($"Detect {filenames.Length} files from arguments.");
+            foreach (var tuple in filenames.WithIndex())
             {
-                Console.WriteLine($"[{index++}/{args.Length}]: {path}");
+                var path = tuple.Item1;
+                var index = tuple.Item2;
+                Console.WriteLine($"[{index + 1}/{filenames.Length}]: {path}");
                 try
                 {
-                    var validGenerator = pdfGenerators.First(generator => generator.IsValidFilename(path));
-                    validGenerator.WritePdfImage(path);
+                    PdfImageGeneratorCollection.WritePdfImage(path);
                     Console.WriteLine($"Succeeded to convert {path} to PDF.");
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine($"Failed to convert {path} to PDF:{e.Message}");
+                    Console.WriteLine($"Failed to convert {path} to PDF\n\n{e.Message}\n\n{e.StackTrace}");
                 }
             }
             Console.WriteLine("All tasks finished.");
         }
-        static IEnumerable<string> getTargetFilenames(IEnumerable<string> args)
+
+        static IEnumerable<string> GetTargetFilenames(IEnumerable<string> args)
         {
             foreach (var arg in args)
             {
